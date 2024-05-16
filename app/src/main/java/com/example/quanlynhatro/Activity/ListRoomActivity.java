bd94b9c9682043819febb0e6ed24b4d032bb26dc
Đặng Huy Diệu <danghuydieu1243@gmail.com>
2024-05-16 14:04:03 +0700

    add_CreateRoomActivity

--- app/src/main/java/com/example/quanlynhatro/Activity/ListRoomActivity.java
+++ app/src/main/java/com/example/quanlynhatro/Activity/ListRoomActivity.java
@@ -1,17 +1,23 @@
 package com.example.quanlynhatro.Activity;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.example.quanlynhatro.Entity.Room;
 import com.example.quanlynhatro.Adapter.ListRoomAdapter;
+import com.example.quanlynhatro.Entity.Tenant;
 import com.example.quanlynhatro.R;
+import com.example.quanlynhatro.database.AppDatabase;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,9 +27,13 @@ public class ListRoomActivity extends AppCompatActivity {
     private ListRoomAdapter listRoomAdapter;
     private List<Room> listRoom;
     private ImageView LR_icon_back;
+    private AppDatabase db;
+    private Button btnCreateRoom;
+
     private void anhxa(){
         rcv_list_room = findViewById(R.id.rcv_list_room);
         LR_icon_back = findViewById(R.id.LR_icon_back);
+        btnCreateRoom = findViewById(R.id.btnCreateRoom);
     }
 
     @Override
@@ -33,6 +43,7 @@ public class ListRoomActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         anhxa();
         setIconBack();
+        createRoomButton();
 
         listRoomAdapter = new ListRoomAdapter();
         listRoom = new ArrayList<>();
@@ -46,11 +57,23 @@ public class ListRoomActivity extends AppCompatActivity {
         listRoom.add(room3);
         listRoom.add(room4);
 
+        db = AppDatabase.getInstance(getApplicationContext());
+        // Kiểm tra nếu db là null
+        if (db == null) {
+            Log.e(TAG, "Cơ sở dữ liệu không được khởi tạo!");
+            return;
+        }
+        List<Room> roomList = db.roomDAO().getAllRooms();
+
+        for (Room r : roomList) {
+            Log.d(TAG, r.toString());
+            listRoom.add(r);
+        }
+
         listRoomAdapter.setData(listRoom);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         rcv_list_room.setLayoutManager(linearLayoutManager);
         rcv_list_room.setAdapter(listRoomAdapter);
-
     }
 
     private void setIconBack(){
@@ -58,6 +81,15 @@ public class ListRoomActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(ListRoomActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+    private void createRoomButton(){
+        btnCreateRoom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ListRoomActivity.this, CreateRoomActivity.class);
                 startActivity(intent);
             }
         });
